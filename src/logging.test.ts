b09a5a9833d5d8ba3a676d51de205bb6d812fc6e
Jean Regisser <jean.regisser@gmail.com>
2022-10-25 11:59:29 +0200

    feat: initial lib implementation (#4)

---
+++ src/logging.test.ts
@@ -0,0 +1,344 @@
+import Logger from 'bunyan'
+import express from 'express'
+import request from 'supertest'
+import { createLogger, createLoggingMiddleware } from './logging'
+import MockDate from 'mockdate'
+
+MockDate.set(new Date('2022-10-18T23:36:07.071Z'))
+
+// @ts-ignore
+const spyLoggerEmit = jest.spyOn(Logger.prototype, '_emit')
+
+const currentProcessEnv = process.env
+
+const DEFAULT_PROPERTY_MATCHER = {
+  hostname: expect.any(String),
+  pid: expect.any(Number),
+}
+
+function globalReplacePhoneNumbers(value: string) {
+  // replaces values that look like phone numbers
+  // `%2B` is the URL encoded version of `+`
+  return value.replace(
+    /(?:\+|%2B)[1-9]\d{1,14}/gi,
+    (phoneNumber) => phoneNumber.slice(0, -4) + 'XXXX',
+  )
+}
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  process.env = { ...currentProcessEnv }
+})
+
+describe('logger', () => {
+  it("should log at the default 'info' level and above", () => {
+    const logger = createLogger()
+    logger.trace('logger.trace')
+    expect(spyLoggerEmit).not.toHaveBeenCalled()
+    logger.debug('logger.debug')
+    expect(spyLoggerEmit).not.toHaveBeenCalled()
+    logger.info('logger.info')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 30, msg: 'logger.info' }),
+    )
+    logger.warn('logger.warn')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 40, msg: 'logger.warn' }),
+    )
+    logger.error('logger.error')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 50, msg: 'logger.error' }),
+    )
+    logger.fatal('logger.fatal')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 60, msg: 'logger.fatal' }),
+    )
+  })
+
+  it('should log at the provided level and above', () => {
+    const logger = createLogger({ level: 'warn' })
+    logger.trace('logger.trace')
+    expect(spyLoggerEmit).not.toHaveBeenCalled()
+    logger.debug('logger.debug')
+    expect(spyLoggerEmit).not.toHaveBeenCalled()
+    logger.info('logger.info')
+    expect(spyLoggerEmit).not.toHaveBeenCalled()
+    logger.warn('logger.warn')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 40, msg: 'logger.warn' }),
+    )
+    logger.error('logger.error')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 50, msg: 'logger.error' }),
+    )
+    logger.fatal('logger.fatal')
+    expect(spyLoggerEmit).toHaveBeenCalledWith(
+      expect.objectContaining({ level: 60, msg: 'logger.fatal' }),
+    )
+  })
+
+  it('should redact specific keys', async () => {
+    const logger = createLogger({
+      redact: {
+        paths: ['a.*.c'],
+      },
+    })
+    logger.info(
+      { a: { b: { c: 'Call me at +1234567890' } } },
+      "I'm a phone number +1234567890",
+    )
+    expect(spyLoggerEmit).toHaveBeenCalledTimes(1)
+    expect(spyLoggerEmit.mock.calls[0][0]).toMatchInlineSnapshot(
+      DEFAULT_PROPERTY_MATCHER,
+      `
+      {
+        "a": {
+          "b": {
+            "c": "[REDACTED]",
+          },
+        },
+        "hostname": Any<String>,
+        "level": 30,
+        "msg": "I'm a phone number +1234567890",
+        "name": "default",
+        "pid": Any<Number>,
+        "time": 2022-10-18T23:36:07.071Z,
+        "v": 0,
+      }
+    `,
+    )
+  })
+
+  it('should redact global patterns', async () => {
+    const logger = createLogger({
+      redact: {
+        globalReplace: globalReplacePhoneNumbers,
+      },
+    })
+    logger.info(
+      { a: { b: { c: 'Call me at +1234567890' } } },
+      "I'm a phone number +1234567890",
+    )
+    expect(spyLoggerEmit).toHaveBeenCalledTimes(1)
+    expect(spyLoggerEmit.mock.calls[0][0]).toMatchInlineSnapshot(
+      DEFAULT_PROPERTY_MATCHER,
+      `
+      {
+        "a": {
+          "b": {
+            "c": "Call me at +123456XXXX",
+          },
+        },
+        "hostname": Any<String>,
+        "level": 30,
+        "msg": "I'm a phone number +123456XXXX",
+        "name": "default",
+        "pid": Any<Number>,
+        "time": 2022-10-18T23:36:07.071Z,
+        "v": 0,
+      }
+    `,
+    )
+  })
+
+  it('should redact using a custom censor value', async () => {
+    const logger = createLogger({
+      redact: {
+        paths: ['a.*.c'],
+        censor: '***REDACTED***',
+      },
+    })
+    logger.info(
+      { a: { b: { c: 'Call me at +1234567890' } } },
+      "I'm a phone number +1234567890",
+    )
+    expect(spyLoggerEmit).toHaveBeenCalledTimes(1)
+    expect(spyLoggerEmit.mock.calls[0][0]).toMatchInlineSnapshot(
+      DEFAULT_PROPERTY_MATCHER,
+      `
+      {
+        "a": {
+          "b": {
+            "c": "***REDACTED***",
+          },
+        },
+        "hostname": Any<String>,
+        "level": 30,
+        "msg": "I'm a phone number +1234567890",
+        "name": "default",
+        "pid": Any<Number>,
+        "time": 2022-10-18T23:36:07.071Z,
+        "v": 0,
+      }
+    `,
+    )
+  })
+})
+
+describe('logging middleware', () => {
+  const logger = createLogger({
+    redact: {
+      paths: [
+        'pepper',
+        '*.pepper',
+        'req.*.pepper',
+        'req.headers.authorization',
+        'req.headers.cookie',
+      ],
+      censor: (_value: any) => {
+        return '***REDACTED***'
+      },
+      globalReplace: globalReplacePhoneNumbers,
+    },
+  })
+  const server = express()
+  server.use(createLoggingMiddleware({ projectId: 'test-project', logger }))
+  server.post('/', (req, res) => {
+    // @ts-ignore
+    logger.info({ req }, 'this helps ensure req is logged and not mutated')
+
+    res.status(200).send({ message: 'OK' })
+  })
+
+  it('should log the request details once finished', async () => {
+    await request(server)
+      .post('/?anotherPhone=%2B1234567890')
+      .send({
+        phoneNumber: '+1234567890',
+      })
+      .set('Content-Type', 'application/json')
+      .set('Authorization', 'SECRET_AUTH_HEADER')
+      .expect(200)
+      .expect({ message: 'OK' })
+
+    expect(spyLoggerEmit).toHaveBeenCalledTimes(2)
+    expect(spyLoggerEmit.mock.calls[1][0]).toMatchInlineSnapshot(
+      {
+        ...DEFAULT_PROPERTY_MATCHER,
+        req: {
+          headers: {
+            host: expect.any(String),
+            'x-cloud-trace-context': expect.any(String),
+          },
+          remotePort: expect.any(Number),
+        },
+        res: {
+          // Can't mock the Date there
+          // See https://github.com/sinonjs/fake-timers/issues/344
+          header: expect.any(String),
+        },
+      },
+      `
+      {
+        "hostname": Any<String>,
+        "level": 30,
+        "msg": "Request finished",
+        "name": "default",
+        "pid": Any<Number>,
+        "req": {
+          "headers": {
+            "accept-encoding": "gzip, deflate",
+            "authorization": "***REDACTED***",
+            "connection": "close",
+            "content-length": "29",
+            "content-type": "application/json",
+            "host": Any<String>,
+            "x-cloud-trace-context": Any<String>,
+          },
+          "method": "POST",
+          "query": {
+            "anotherPhone": "+123456XXXX",
+          },
+          "remoteAddress": "::ffff:127.0.0.1",
+          "remotePort": Any<Number>,
+          "url": "/?anotherPhone=%2B123456XXXX",
+        },
+        "res": {
+          "header": Any<String>,
+          "statusCode": 200,
+        },
+        "time": 2022-10-18T23:36:07.071Z,
+        "v": 0,
+      }
+    `,
+    )
+  })
+
+  it('should log the request details with additional fields in a Cloud Functions environment once finished', async () => {
+    // Simulate a Cloud Functions environment
+    process.env.K_SERVICE = 'testLogger'
+
+    await request(server)
+      .post('/?anotherPhone=%2B1234567890')
+      .send({
+        phoneNumber: '+1234567890',
+      })
+      .set('Content-Type', 'application/json')
+      .set('Authorization', 'SECRET_AUTH_HEADER')
+      .expect(200)
+      .expect({ message: 'OK' })
+
+    expect(spyLoggerEmit).toHaveBeenCalledTimes(2)
+    expect(spyLoggerEmit.mock.calls[1][0]).toMatchInlineSnapshot(
+      {
+        ...DEFAULT_PROPERTY_MATCHER,
+        req: {
+          headers: {
+            host: expect.any(String),
+            'x-cloud-trace-context': expect.any(String),
+          },
+          remotePort: expect.any(Number),
+        },
+        res: {
+          // Can't mock the Date there
+          // See https://github.com/sinonjs/fake-timers/issues/344
+          header: expect.any(String),
+        },
+        'logging.googleapis.com/spanId': expect.any(String),
+        'logging.googleapis.com/trace': expect.any(String),
+      },
+      `
+      {
+        "hostname": Any<String>,
+        "httpRequest": {
+          "requestMethod": "POST",
+          "requestUrl": "/?anotherPhone=%2B123456XXXX",
+          "responseSize": 16,
+          "status": 200,
+        },
+        "level": 30,
+        "logging.googleapis.com/spanId": Any<String>,
+        "logging.googleapis.com/trace": Any<String>,
+        "logging.googleapis.com/trace_sampled": false,
+        "msg": "Request finished",
+        "name": "default",
+        "pid": Any<Number>,
+        "req": {
+          "headers": {
+            "accept-encoding": "gzip, deflate",
+            "authorization": "***REDACTED***",
+            "connection": "close",
+            "content-length": "29",
+            "content-type": "application/json",
+            "host": Any<String>,
+            "x-cloud-trace-context": Any<String>,
+          },
+          "method": "POST",
+          "query": {
+            "anotherPhone": "+123456XXXX",
+          },
+          "remoteAddress": "::ffff:127.0.0.1",
+          "remotePort": Any<Number>,
+          "url": "/?anotherPhone=%2B123456XXXX",
+        },
+        "res": {
+          "header": Any<String>,
+          "statusCode": 200,
+        },
+        "time": 2022-10-18T23:36:07.071Z,
+        "v": 0,
+      }
+    `,
+    )
+  })
+})
---
+++ src/logging.ts
@@ -0,0 +1,192 @@
+import Logger, { LogLevel } from 'bunyan'
+import PrettyStream from 'bunyan-prettystream'
+import {
+  HttpRequest,
+  middleware as commonMiddleware,
+} from '@google-cloud/logging'
+import {
+  LoggingBunyan,
+  LOGGING_SAMPLED_KEY,
+  LOGGING_SPAN_KEY,
+  LOGGING_TRACE_KEY,
+} from '@google-cloud/logging-bunyan'
+import { ServerResponse } from 'http'
+import { ServerRequest } from '@google-cloud/logging/build/src/utils/http-request'
+import fastRedact from 'fast-redact'
+
+// GAE_SERVICE is the service name of the App Engine service:
+//   https://cloud.google.com/appengine/docs/standard/nodejs/runtime#environment_variables
+// We also use it to infer we're running in an App Engine environment.
+// K_SERVICE name of function resource on a Cloud Function:
+//   https://cloud.google.com/functions/docs/configuring/env-var#newer_runtimes
+// We also use it to infer we're running in a Cloud Function environment.
+function getGoogleServiceName() {
+  return process.env.GAE_SERVICE || process.env.K_SERVICE
+}
+
+interface ExtendedRedactOptions extends fastRedact.RedactOptions {
+  // Allows to globally replace sensitive patterns
+  // WARNING: the value is JSON.stringified before being passed to this function
+  // and will be JSON.parse'd after
+  globalReplace?: (value: string) => string
+}
+
+export function createLogger({
+  level,
+  redact: redactConfig,
+}: {
+  level?: LogLevel
+  redact?: ExtendedRedactOptions
+} = {}) {
+  const logLevel = level || (process.env.LOG_LEVEL as LogLevel) || 'info'
+  const streams: Logger.Stream[] = []
+  let name = 'default'
+
+  const googleServiceName = getGoogleServiceName()
+
+  if (googleServiceName) {
+    // https://github.com/googleapis/nodejs-logging-bunyan/issues/304
+    // https://github.com/googleapis/nodejs-logging-bunyan#alternative-way-to-ingest-logs-in-google-cloud-managed-environments
+    // redirectToStdout helps ensure the logging stream is flushed before the process exists.
+    // useMessageField must be `false` for Logs Explorer to show the string message as a log entry line
+    // Otherwise it nests everything under `jsonPayload.message` and all lines in Logs Explorer look like JSON noise.
+    const loggingBunyan = new LoggingBunyan({
+      redirectToStdout: true,
+      useMessageField: false,
+    })
+    name = googleServiceName
+    streams.push(loggingBunyan.stream(logLevel))
+  } else {
+    const consoleStream = new PrettyStream({ mode: 'short' })
+    consoleStream.pipe(process.stdout)
+    streams.push({ stream: consoleStream, level: logLevel })
+  }
+
+  const logger = Logger.createLogger({
+    name,
+    streams,
+    serializers: createDetailedRequestSerializers(),
+  })
+
+  const redact = fastRedact({ ...redactConfig, serialize: false })
+
+  // Patch _emit to redact sensitive data
+  // This redacts **all** fields in the log record, not just the ones we specify in the serializers
+  // @ts-expect-error
+  logger._emit = new Proxy(logger._emit, {
+    apply: function (target, thisArgument, argumentsList) {
+      const [logRecord] = argumentsList
+
+      const globalReplace =
+        redactConfig?.globalReplace ?? ((value: string) => value)
+
+      // Preserve bunyan's core fields (except for msg)
+      // See https://github.com/trentm/node-bunyan#core-fields
+      const { v, level, name, hostname, pid, time, src, ...rest } = logRecord
+
+      // redact mutates the input object,
+      // so here we copy it and overwrite the log record with the redacted copied version
+      // This makes the redact action stable when calling `logger.info({ req })` multiple times
+      // i.e. the original `req` object is not mutated
+      // This assumes all fields are serializable, which they should at this point
+      Object.assign(
+        logRecord,
+        redact(JSON.parse(globalReplace(JSON.stringify(rest)))),
+      )
+
+      // Call the original _emit
+      return Reflect.apply(target, thisArgument, argumentsList)
+    },
+  })
+
+  return logger
+}
+
+// Adapted from https://github.com/googleapis/nodejs-logging-bunyan/blob/4de2b3dd9e8f6b336d9ca3609f775046a6f74424/src/middleware/express.ts
+// This logs the request and response objects for all requests.
+// It also shows nicely formatted request logs for Cloud Functions in Logs Explorer (App Engine does this automatically).
+export function createLoggingMiddleware({
+  projectId,
+  logger,
+}: {
+  projectId: string
+  logger: Logger
+}) {
+  function makeChildLogger(trace: string, span?: string) {
+    return logger.child(
+      { [LOGGING_TRACE_KEY]: trace, [LOGGING_SPAN_KEY]: span },
+      true /* simple child */,
+    )
+  }
+
+  return (req: ServerRequest, res: ServerResponse, next: Function) => {
+    const emitRequestLog = (
+      httpRequest: HttpRequest,
+      trace: string,
+      span?: string,
+      sampled?: boolean,
+    ) => {
+      logger.info(
+        {
+          // Log more info about the request
+          // See also the serializers for these fields
+          req,
+          res,
+          // Note: Contrary to what the documentation says for `makeMiddleware`,
+          // Cloud Functions (at least the gen1 version we use) doesn't already log the httpRequest
+          // So we do it ourselves
+          ...(process.env.K_SERVICE
+            ? {
+                // This shows the nicely formatted request log in Logs Explorer.
+                // See https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#HttpRequest
+                httpRequest,
+                [LOGGING_TRACE_KEY]: trace,
+                [LOGGING_SPAN_KEY]: span,
+                [LOGGING_SAMPLED_KEY]: sampled,
+              }
+            : undefined),
+        },
+        'Request finished',
+      )
+    }
+    commonMiddleware.express.makeMiddleware(
+      projectId,
+      makeChildLogger,
+      emitRequestLog,
+    )(req, res, next)
+  }
+}
+
+// Similar to the stdSerializers in bunyan, but with a few extra fields (query and body mostly)
+export function createDetailedRequestSerializers() {
+  const serializers: Logger.Serializers = { err: Logger.stdSerializers.err }
+
+  serializers.req = (req: any) => {
+    if (!req || !req.connection) {
+      return req
+    }
+    return {
+      method: req.method,
+      // Accept `req.originalUrl` for expressjs usage.
+      // https://expressjs.com/en/api.html#req.originalUrl
+      url: req.originalUrl || req.url,
+      query: req.query,
+      body: req.body,
+      headers: req.headers,
+      remoteAddress: req.connection.remoteAddress,
+      remotePort: req.connection.remotePort,
+    }
+  }
+
+  serializers.res = (res: any) => {
+    if (!res || !res.statusCode) {
+      return res
+    }
+    return {
+      statusCode: res.statusCode,
+      header: res._header,
+    }
+  }
+
+  return serializers
+}
